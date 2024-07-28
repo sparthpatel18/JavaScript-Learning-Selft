389cc2fb60b3a83a8eb18de76eeed5dcac6f5f1c
parthpatel <sparthpatel18@gmail.com>
2024-07-28 11:06:23 -0400

    Variable Commit

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail = "parth@google.com"
+let accountNmae = "parth78"
+let accountCity = "Ottawa"
+
+console.log(accountNmae);
+console.log(accountCity);
+console.log(accountEmail);
+
+console.log([accountCity,accountEmail,accountId,accountNmae]);
